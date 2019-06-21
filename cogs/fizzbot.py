c8f7b017ffe08ed52cf2329379ed891768ae5d53
Mik <mik23@protonmail.ch>
2019-06-22 02:35:05 +0530

    moveed logic and fixed fixxbot

--- cogs/fizzbot.py
+++ cogs/fizzbot.py
@@ -1,20 +1,65 @@
+from discord.ext import commands
+from logic import fizz
 import requests as rq
-import json
+import discord.utils
+import discord
 
-class fizz:
-    def __init__(self):
-        self.url = "https://api.noopschallenge.com"
-        self.next = "/fizzbot"
-        self.burl = self.url+self.next
+client = discord.Client()
+bot = commands.Bot(command_prefix='noops ', description='Discord hackweek meets noops...')
 
-    def get_question(self):
-        self.burl = self.url+self.next
-        noop = rq.get(self.burl).json()
-        message = noop['message']
-        self.next = noop['nextQuestion']
-        return message, self.next
+class Nooper:
+    def __init__(self, bot):
+        self.bot = bot
+        self.noop = fizz()
+        self.qlock = False
+        self.start = False
+        self.question = ''
 
-    def send_answer(self, answer):
-        data = json.dumps({'answer':answer})
-        code = rq.post(self.burl, data=data)
-        return code
+    @bot.command(pass_context=True, name="start")
+    async def _start(self, ctx):
+        """
+            Start the noops challenge
+        """
+        if not self.start:
+            try:
+                self.question = self.noop.get_question()
+                await ctx.send(self.question)
+                await ctx.send('Noops challenge started')
+                self.start = True
+            except Exception as e:
+                await ctx.send('something went wong in ask {}'.format(e))
+        else:
+            await ctx.send('The noops challenge is already running in this server')
+
+    @bot.command(pass_context=True)
+    async def ask(self, ctx):
+        """
+            The noops bot asks you a question.
+        """
+        try:
+            if not self.qlock and self.start:
+                self.question = self.noop.get_question()
+                await ctx.send(self.question)
+            else:
+                await ctx.send('Answer the previous question...\n {}'.format(self.question))
+        except Exception as e:
+            await ctx.send('something went wong in ask {}'.format(e))
+
+    @bot.command(pass_context=True)
+    async def send(self, ctx, *, message):
+        """
+            You answer the noops bot's question.
+        """
+        try:
+            code = self.noop.send_answer(message)
+            if code['result'] == 'correct':
+                await ctx.send("{}".format(code['message']))
+                self.qlock = False
+            else:
+                await ctx.send("incorrect answer, try again... \n QUESTION \n{}".format(self.question))
+                self.qlock = True
+        except Exception as e:
+            await ctx.send('something went wong in send {}'.format(e))
+
+def setup(bot):
+    bot.add_cog(Nooper(bot))
