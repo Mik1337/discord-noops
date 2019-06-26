5547d77bcff077baf3b4d29aa51bf3e80b06625f
Mik <mik23@protonmail.ch>
2019-06-26 20:29:07 +0530

    async overhaul, still gotta fix tho

--- logic.py
+++ logic.py
@@ -7,22 +7,24 @@ class fizz:
         self.next = "/fizzbot"
         self.burl = self.url+self.next
 
-    def get_question(self):
+    async def get_question(self):
         noop = rq.get(self.burl).json() # get the response
         # message = noop['message'] # message from response
         # rules = noop['rules']
         # numbers = noop['numbers']
         # self.next = noop['nextQuestion'] # next quesion link
         self.burl = "https://api.noopschallenge.com/fizzbot/questions/1"
-        return noop
+        return noop # return question ?
+        # await noop # return question ?
 
-    def send_answer(self, answer):
+    async def send_answer(self, answer):
         data = json.dumps({'answer':answer}) # json-ify dict for post
         code = rq.post(self.burl, data=data).json() # post answer
         if code['result'] == 'correct':
             self.next = code['nextQuestion']
             self.burl = self.url + self.next
         return code # return json of response
+        # await code # return json of response
 
-    def clear(self):
+    async def clear(self):
         self.next = '/fizzbot' # reset var
